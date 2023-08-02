c338f96c242ed87586b6ac3394916f1d17e917b2
axel <axosar@gmail.com>
2023-08-01 18:57:41 -0600

    custom-basic-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MY ANGULAR RCOVERING';
+  title = 'MY ANGULAR RECOVERING';
   name = 'AXEL';
 }
